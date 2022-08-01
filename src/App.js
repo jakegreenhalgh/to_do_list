8dfe697c45afcc548bb3465db0378722d38ef655
Jacob <jacob_greenhalgh@hotmail.com>
2022-08-01 14:55:35 +0100

    added css code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,90 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    { name: "Buy shopping", priority: "high" },
+    { name: "Clean bathroom", priority: "low" },
+    { name: "Car's MOT", priority: "high" }
+  ]);
+  const [newName, setNewTask] =useState("");
+
+  const [newPriority, setNewPriority] = useState("")
+  
+  const isHighPriority = (priority)=> {
+      if (priority === "high") {
+        return true
+      } else {
+        return false
+      }}
+  
+  const taskNodes = tasks.map( (task, index) => {
+    return (
+      <li key={index} className={ isHighPriority(task.priority) ? "high-priority" : "low-priority"} >
+        <span>
+          { task.name }
+        </span>
+        {isHighPriority(task.priority) ? <span className="high-priority">High</span> : <span className="low-priority">Low</span>}
+      </li>
+    )
+  });
+
+  
+
+  const handleTaskInput = (event) => {
+    setNewTask(event.target.value);
+  }
+  const handlePriorityInput = (event) => {
+    setNewPriority(event.target.value);
+  }
+  
+  const saveNewTask = (event) => {
+    event.preventDefault();
+    const copyTasks = [...tasks];
+    copyTasks.push({
+      name: newName,
+      priority: newPriority
+    })
+    setTasks(copyTasks);
+    setNewTask("")
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1> To-Do's</h1>
+        <form onSubmit={saveNewTask}>
+        <input id='new_task'
+        type="text"
+        value={newName}
+        onChange= {handleTaskInput} ></input>
+        <div onChange= {handlePriorityInput}>
+        <label htmlFor='low-priority'>Low Priority</label>  
+        <input 
+        type='radio' 
+        id='low-priority'
+        name='priority'
+        value='low'>
+        </input>
+        <label htmlFor='high-priority'>High Priority</label>
+        <input 
+        type='radio' 
+        id='high-priority'
+        name='priority'
+        value='high'>
+        </input>
+        </div>
+        <input
+        type="submit"
+        value="Save new task">
+
+        </input>
+        </form>
+        
+        
+        <ul>{taskNodes}</ul>
+        
+
     </div>
   );
 }
